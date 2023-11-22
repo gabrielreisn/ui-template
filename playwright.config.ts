2905d28357bdee9e4274ec4fecaf3f4e7d4f6861
gabriel reis <gabrielreisnf@gmail.com>
2023-11-22 15:03:26 -0300

    chore: add github actions scripts and update commands

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   retries: 2,
   // Run your local dev server before starting the tests
   webServer: {
-    command: 'npm run start',
+    command: 'pnpm run dev',
     url: 'http://127.0.0.1:3000',
     reuseExistingServer: !process.env.CI,
     stdout: 'ignore',
